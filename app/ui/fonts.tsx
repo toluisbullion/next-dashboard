05e89b9d6e41703020607954c958395db7230ebc
Niyi <Niyi@Joshuas-MacBook-Air.local>
2025-10-06 13:54:45 +0100

    Initial commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+// import { lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
